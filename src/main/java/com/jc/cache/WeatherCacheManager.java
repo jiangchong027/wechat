c689c598369092222a93c1906885e2bcf2baad2c
chongjiang <54271523@qq.com>
2017-12-09 20:59:03 +0800

    Init commit.

--- src/main/java/com/jc/cache/WeatherCacheManager.java
+++ src/main/java/com/jc/cache/WeatherCacheManager.java
@@ -21,52 +21,52 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class WeatherCacheManager {
 
-	@Autowired
-	private WeatherApi weatherApi;
+    @Autowired
+    private WeatherApi weatherApi;
 
-	private Map<String, WeatherCache> weatherCacheMap = new ConcurrentHashMap<>();
+    private Map<String, WeatherCache> weatherCacheMap = new ConcurrentHashMap<>();
 
-	/**
-	 * 根据key获取对应缓存
-	 *
-	 * @param city   城市
-	 * @param expire 过期时间
-	 * @return cache
-	 */
-	public WeatherCache getData(String city, long expire) {
-		WeatherCache weatherCache = weatherCacheMap.get(city);
-		if (Objects.isNull(weatherCache)) {
-			Weather weather = weatherApi.getWeather(city);
-			weatherCache = new WeatherCache(weather, expire);
-			weatherCacheMap.put(city, weatherCache);
-			return weatherCache;
-		}
-		Date expireTime = new Date(weatherCache.getExpire());
-		Date now = new Date();
-		if (expireTime.before(now)) {
-			Weather weather = weatherApi.getWeather(city);
-			weatherCache = new WeatherCache(weather, expire);
-			weatherCacheMap.put(city, weatherCache);
-		}
-		return weatherCache;
-	}
+    /**
+     * 根据key获取对应缓存
+     *
+     * @param city   城市
+     * @param expire 过期时间
+     * @return cache
+     */
+    public WeatherCache getData(String city, long expire) {
+        WeatherCache weatherCache = weatherCacheMap.get(city);
+        if (Objects.isNull(weatherCache)) {
+            Weather weather = weatherApi.getWeather(city);
+            weatherCache = new WeatherCache(weather, System.currentTimeMillis() + expire);
+            weatherCacheMap.put(city, weatherCache);
+            return weatherCache;
+        }
+        Date expireTime = new Date(weatherCache.getExpire());
+        Date now = new Date();
+        if (expireTime.before(now)) {
+            Weather weather = weatherApi.getWeather(city);
+            weatherCache = new WeatherCache(weather, System.currentTimeMillis() + expire);
+            weatherCacheMap.put(city, weatherCache);
+        }
+        return weatherCache;
+    }
 
-	/**
-	 * @param city 城市
-	 * @return cache
-	 */
-	public WeatherCache getData(String city) {
-		return getData(city, getDefaultExpireTime());
-	}
+    /**
+     * @param city 城市
+     * @return cache
+     */
+    public WeatherCache getData(String city) {
+        return getData(city, getDefaultExpireTime());
+    }
 
-	private long getDefaultExpireTime() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.MILLISECOND, 1);
-		calendar.add(Calendar.DATE, 1);
-		return calendar.getTime().getTime();
-	}
+    private long getDefaultExpireTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.MILLISECOND, 1);
+        calendar.add(Calendar.DATE, 1);
+        return calendar.getTime().getTime();
+    }
 
 }
